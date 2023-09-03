7476d2ad8d55cc4e0be77f744b0106af27d27adc
bbeeeeenn <124857903+bbeeeeenn@users.noreply.github.com>
2023-09-04 03:05:57 +0800

    .

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+const fact = document.getElementById("catFact");
+const randomizer = document.querySelector(".randomizer");
+getRandomCatFact();
+randomizer.addEventListener("click", () => {
+	getRandomCatFact();
+	enableRandomize = false;
+});
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		getRandomCatFact();
+		enableRandomize = false;
+	}
+});
+
+async function getRandomCatFact() {
+	container.classList.add("shrunk");
+	fact.innerText = "Nyaw...";
+	const response = await fetch(`https://catfact.ninja/fact?max_length=140`);
+	const data = await response.json();
+	console.log(data);
+	container.classList.remove("shrunk");
+	fact.innerText = data.fact;
+}
